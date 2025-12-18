49fc9494bcbadc35ec66bc749799a4c63caa33b4
Aykachasanli <hesenliaykac@gmail.com>
2025-12-18 19:47:34 +0400

    umumi

--- backend_server/routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getCart, addToCart } = require("../controllers/cartController");
-
-router.get("/cart/:userId", getCart);
-
-router.post("/cart", addToCart);
-
-module.exports = router;
