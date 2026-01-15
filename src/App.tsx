5dacfbca16701cd9ab2a50779821567c461b61a3
Aykachasanli <hesenliaykac@gmail.com>
2026-01-15 20:48:16 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ScrollToTop from "./components/ScrollToTop";
-
 import PascaleRouter from "./router/PascaleRouter";
-import BuyArtworkModal from "./components/BuyArtworkModal"; 
 import FloatingSearch from "./components/FloatingSearch";
+import BuyArtworkModal from "./components/BuyArtworkModal";
 export default function App() {
   return (
     <>
